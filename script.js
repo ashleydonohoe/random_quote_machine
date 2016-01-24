c4009b2dd496082d15ac479ff72e78906a41a0e3
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-24 01:26:44 -0500

    added rest of quotes and fixed styling

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 var availableQuotes = [
     { quote: "The best preparation for tomorrow is doing your best today.", author: "H. Jackson Brown, Jr."},
     { quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.", author: "Francis of Assisi"},
-    { quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.". author: "Helen Keller"},
+    { quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", author: "Helen Keller"},
     { quote: "Nothing is impossible, the word itself says 'I'm possible'!", author: "Audrey Hepburn"},
-
+    { quote: "We know what we are, but know not what we may be.", author: "Shakespeare"},
+    { quote: "No act of kindness, no matter how small, is ever wasted.", author: "Aesop"},
+    {quote: "A hero is someone who has given his or her life to something bigger than oneself.", author: "Joseph Campbell"},
+    { quote: "You must do the things you think you cannot do.", author: "Eleanor Roosevelt"},
+    { quote: "If you believe in yourself and have dedication and pride - and never quit, you'll be a winner. The price of victory is high but so are the rewards.", author: "Paul Bryant"}
 ];
 
 // Class representing a single quote
